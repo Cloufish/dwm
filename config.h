043e531d1dce9a80663413579c665476fbad020f
Cloufish <cloufish@protonmail.com>
2022-09-18 15:07:01 +0200

    Disabled StatusBar, decreased gaps, changed browser

--- config.h
+++ config.h
@@ -3,18 +3,18 @@
 /* Constants */
 #define TERMINAL "st"
 #define TERMCLASS "St"
-#define BROWSER "librewolf"
+#define BROWSER "brave"
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int snap      = 10;       /* snap pixel */
+static unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
+static int showbar            = 0;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
